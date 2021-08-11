258108dcf3551a18523bfeea126806bd0841d558
janakmpatel <88542172+janakmpatel@users.noreply.github.com>
2021-08-11 17:02:26 +0530

    Add files via upload

---
+++ Hookes_jeevs.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# -*- HV method by Team 12  -*-
+import time 
+intial=time.time()
+import numpy as np
+import matplotlib.pyplot as plt
+#function
+def func(x):
+    fx = 100*((x[1]-x[0]**2)**2)+((1-x[0])**2)
+    #fx=((x[0]-3)**2)+((1+x[1])**2)
+    #fx = 100*((2+x[1]-x[0]**2)**2)+((3-x[0])**2)
+    #fx = 100*((2+x[1]**2-x[0]**3)**2)+((x[1]+5-x[0])**2)
+    return fx
+#FV method
+def HV(func,x_init,max_iter,epsilon,delta,alpha):
+    k=0
+    it =0
+    xk=x_init
+    x_vec_save=[]
+    f_vec_save=[]
+    x_vec_save.append(x_init)             #To create array of all x at different iteration
+    f_vec_save.append(func(x_init))
+    n=x_init.shape[0]
+    print(n)
+    e=np.eye(n)
+    while delta>epsilon and k<max_iter:
+        for i in range(n):               #exploratory move
+            xi = xk
+            he=delta*(np.vstack(e[:,i]))
+            x_p=xk+he
+            x_n=xk-he
+            f=func(xk)
+            f1=func(x_p)
+            f2=func(x_n)
+            if f1<f:
+                if f1<f2:
+                    xk_1=x_p
+                else:
+                    xk_1=x_n
+            elif f2<f:
+                xk_1=x_n
+            else:
+                xk_1=xk
+            xk=xk_1
+          
+        if (xk_1).all==(xi).all:
+            delta=delta/alpha
+        else:
+            
+            sk = xk_1-xi
+                #pattern move 
+            xk_2 = xk_1+sk
+            f_new = func(xk_2)
+            f_old = func(xk_1)
+            if f_new<f_old:
+                x_converged=xk_2
+                xk_1 = xk_2
+            else:
+                x_converged = xk_1
+            
+        it=it+1
+        xk = x_converged
+        x_vec_save= np.append(x_vec_save,x_converged)
+        f_save = func(x_converged)
+        f_vec_save= np.append(f_vec_save,f_save)
+    return(x_converged,x_vec_save,f_vec_save,it)
+#PARAmeters     
+delta=1
+alpha=2
+epsilon=10**(-6)
+#Evaluating
+max_iter=15000
+x_init=np.vstack([1.5,1.5])
+(x_converged,x_vec_save,f_vec_save,it)=HV(func,x_init,max_iter,epsilon,delta,alpha)
+print(x_converged)
+print(it)
+
+
+x1 = []
+x2 = []
+for i in range(2*it):
+ if (i%2==0):  
+  x1_vec = x_vec_save[i]
+  x1 = np.append(x1,x1_vec)
+ else:
+     x2_vec = x_vec_save[i]
+     x2 = np.append(x2,x2_vec)
+print(x_converged)
+print(it)
+##plotting
+plt.figure(1)
+k = np.linspace(0,it-1,it) 
+plt.plot(k,x1,label = 'x1',marker = 'x', markersize = '2')
+plt.plot(k,x2,label ='x2', marker = 'o', markersize = '2')
+plt.xlabel('iteration number')
+plt.ylabel('x')
+plt.legend()
+
+plt.figure(2)
+n = np.linspace(0,it,it+1)
+plt.plot(n,f_vec_save, marker = '*')
+plt.xlabel('iteration number')
+plt.ylabel('function value')
+plt.show()
+final=time.time()
+print(final-intial)
